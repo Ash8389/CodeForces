f3c26b5c5e87cf076d88a5e8d5d27c77bc1d0387
Ashish Jha <jhaashish270@gmail.com>
2025-12-19 21:35:08 +0530

    New Question Added

---
+++ A_Operations_with_Inversions.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int mx = a[0];
+    int cnt = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (mx > a[i])
+        {
+            cnt++;
+        }
+        else
+        {
+            mx = a[i];
+        }
+    }
+
+    cout << cnt << "\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
